ee9d3e3b681e0ab45ebc922386dce165e0391b96
MalasTripas123 <g.vieirabert@gmail.com>
2026-04-14 16:24:08 -0400

    Asalto como counter y cambio de pov

--- to-do-list.js
+++ to-do-list.js
@@ -1,14 +1,14 @@
 // bajar el precio del mapa completo cada vez que se vende y barajar
 // destruir reliquias al venderlas
 // barajar las cartas especiales al venderlas (mapa completo/incompleto y papel mágico)
-//¿ - ordenar mano o permitir al jugador ordenar su mano
 // - asalto al explorar
-//¿ - asalto como counter
+// - asalto como counter
 // embaucar
 //? - zona de contador de pistas vendidas
-//¿ - papel mágico
-//¿ - victoria
+//¿ - papel mágico (lógica y visual)
+//¿ - victoria (+ notificación de final)
 //¿ - derrota al pasar el turno sin monedas
+//¿ - ordenar mano o permitir al jugador ordenar su mano
 //¿ - 
 //¿ - 
 
